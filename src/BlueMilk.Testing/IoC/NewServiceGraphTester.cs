5616546dd8e65ebb4be3b4eb930f410ee13bce85
Jeremy D. Miller <jeremydmiller@yahoo.com>
2018-01-03 10:40:24 -0600

    Starting the NewServiceGraph

---
+++ src/BlueMilk.Testing/IoC/NewServiceGraphTester.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using BlueMilk.Codegen;
+using BlueMilk.IoC.Instances;
+using BlueMilk.Testing.TargetTypes;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using Xunit;
+
+namespace BlueMilk.Testing.IoC
+{
+    public class NewServiceGraphTester
+    {
+        public readonly ServiceRegistry theServices = new ServiceRegistry();
+
+        public NewServiceGraphTester()
+        {
+            
+        }
+        
+        [Fact]
+        public void finds_the_single_default()
+        {
+            theServices.AddTransient<IWidget, AWidget>();
+            var theGraph = new NewServiceGraph(theServices);
+
+            theGraph.FindDefault(typeof(IWidget))
+                .ShouldBeOfType<ConstructorInstance>()
+                .ImplementationType.ShouldBe(typeof(AWidget));
+        }
+
+        [Fact]
+        public void finds_the_last_as_the_default()
+        {
+            theServices.AddTransient<IWidget, AWidget>();
+            theServices.AddSingleton(this);
+            theServices.AddTransient<IGeneratedMethod, GeneratedMethod>();
+            theServices.AddTransient<IWidget, MoneyWidget>();
+            
+            var theGraph = new NewServiceGraph(theServices);
+
+            theGraph.FindDefault(typeof(IWidget))
+                .ImplementationType.ShouldBe(typeof(MoneyWidget));
+
+        }
+
+        [Fact]
+        public void finds_all()
+        {
+            theServices.AddTransient<IWidget, AWidget>();
+            theServices.AddSingleton(this);
+            theServices.AddTransient<IGeneratedMethod, GeneratedMethod>();
+            theServices.AddTransient<IWidget, MoneyWidget>();
+            
+            var theGraph = new NewServiceGraph(theServices);
+
+            theGraph.FindAll(typeof(IWidget))
+                .Select(x => x.ImplementationType)
+                .ShouldHaveTheSameElementsAs(typeof(AWidget), typeof(MoneyWidget));
+
+        }
+
+    }
+}
---
+++ src/BlueMilk/NewServiceGraph.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using Baseline;
+using BlueMilk.IoC.Instances;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlueMilk
+{
+    public class NewServiceGraph
+    {
+        private readonly object _familyLock = new object();
+        public readonly IServiceCollection Services;
+        private readonly Dictionary<Type, ServiceFamily> _families = new Dictionary<Type, ServiceFamily>();
+        private readonly IList<IFamilyPolicy> _familyPolicies = new List<IFamilyPolicy>();
+        
+        public NewServiceGraph(IServiceCollection services)
+        {
+            // TODO -- will need to be able to use custom family policies
+            
+            
+            Services = services;
+
+            services
+                .Where(x => !x.ServiceType.IsGenericType)
+                .Select(Instance.For)
+                .GroupBy(x => x.ServiceType)
+                .Select(x => new ServiceFamily(x.Key, x.ToArray()))
+                .Each(family => _families.Add(family.ServiceType, family));
+        }
+
+        public IReadOnlyDictionary<Type, ServiceFamily> Families => _families;
+
+        public ServiceFamily FindFamily(Type serviceType)
+        {
+            if (_families.ContainsKey(serviceType)) return _families[serviceType];
+
+            lock (_familyLock)
+            {
+                if (_families.ContainsKey(serviceType)) return null;
+                
+                var family = _familyPolicies.FirstValue(x => x.Build(serviceType, this));
+                _families.Add(serviceType, family); // Legal to be null here
+
+                return family;
+            }
+        }
+        
+        public Instance FindDefault(Type serviceType)
+        {
+            return FindFamily(serviceType)?.Default;
+        }
+
+        public Instance[] FindAll(Type serviceType)
+        {
+            return FindFamily(serviceType)?.Instances.Values.ToArray() ?? new Instance[0];
+        }
+    }
+
+    public class ResolverGraph
+    {
+        
+    }
+
+    public class ServiceFamily
+    {
+        private readonly Dictionary<string, Instance> _instances = new Dictionary<string, Instance>();
+        
+        
+        public Type ServiceType { get; }
+
+        public ServiceFamily(Type serviceType, Instance[] instances)
+        {
+            ServiceType = serviceType;
+
+            Default = instances.Last();
+
+            makeNamesUnique(instances);
+
+            foreach (var instance in instances)
+            {
+                _instances.Add(instance.Name, instance);
+            }
+        }
+
+        private void makeNamesUnique(IEnumerable<Instance> instances)
+        {
+            // TODO -- add suffixes where necessary
+        }
+
+        public Instance Default { get; }
+
+        public IReadOnlyDictionary<string, Instance> Instances => _instances;
+    }
+}
