3ce94dbe01d18a2e2f2a020f24feb4a0759f52d8
Jeremy D. Miller <jeremydmiller@yahoo.com>
2018-01-03 09:59:07 -0600

    Instance classes and Instance.For(descriptor)

---
+++ src/BlueMilk.Testing/IoC/Instances/basic_instance_behavior.cs
@@ -0,0 +1,57 @@
+﻿using BlueMilk.IoC.Instances;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using Xunit;
+
+namespace BlueMilk.Testing.IoC.Instances
+{
+    public class basic_instance_behavior
+    {
+        [Fact]
+        public void for_returns_the_instance()
+        {
+            var @object = new ObjectInstance(typeof(string),"foo");
+            Instance.For(@object)
+                .ShouldBeSameAs(@object);
+        }
+
+        [Fact]
+        public void for_object()
+        {
+            var descriptor = ServiceDescriptor.Singleton(typeof(string), "foo");
+            var instance = Instance.For(descriptor)
+                .ShouldBeOfType<ObjectInstance>();
+            
+            instance.ImplementationInstance.ShouldBe(descriptor.ImplementationInstance);
+            instance.ServiceType.ShouldBe(descriptor.ServiceType);
+            instance.Lifetime.ShouldBe(descriptor.Lifetime);
+        }
+        
+        [Fact]
+        public void for_concrete_type()
+        {
+            var descriptor = ServiceDescriptor.Scoped<IClock, DisposableClock>();
+            var instance = Instance.For(descriptor)
+                .ShouldBeOfType<ConstructorInstance>();
+            
+            instance.ImplementationType.ShouldBe(descriptor.ImplementationType);
+            instance.ServiceType.ShouldBe(descriptor.ServiceType);
+            instance.Lifetime.ShouldBe(descriptor.Lifetime);
+        }
+        
+        [Fact]
+        public void for_lambda()
+        {
+            var descriptor = ServiceDescriptor.Singleton<IClock>(s => new Clock());
+            
+            var instance = Instance.For(descriptor)
+                .ShouldBeOfType<LambdaInstance>();
+            
+            instance.ImplementationFactory.ShouldBe(descriptor.ImplementationFactory);
+            instance.ServiceType.ShouldBe(descriptor.ServiceType);
+            instance.Lifetime.ShouldBe(descriptor.Lifetime);
+        }
+        
+        
+    }
+}
---
+++ src/BlueMilk/IoC/Instances/Instance.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlueMilk.IoC.Instances
+{
+    public abstract class Instance : ServiceDescriptor
+    {
+        public static Instance For(ServiceDescriptor service)
+        {
+            if (service is Instance instance) return instance;
+            
+            if (service.ImplementationInstance != null) return new ObjectInstance(service.ServiceType, service.ImplementationInstance);
+            
+            if (service.ImplementationFactory != null) return new LambdaInstance(service.ServiceType, service.ImplementationFactory, service.Lifetime);
+
+            return new ConstructorInstance(service.ServiceType, service.ImplementationType, service.Lifetime);
+        }
+        
+        protected Instance(Type serviceType, Type implementationType, ServiceLifetime lifetime) : base(serviceType, implementationType, lifetime)
+        {
+        }
+
+        protected Instance(Type serviceType, object instance) : base(serviceType, instance)
+        {
+        }
+
+        protected Instance(Type serviceType, Func<IServiceProvider, object> factory, ServiceLifetime lifetime) : base(serviceType, factory, lifetime)
+        {
+        }
+
+        public string Name { get; set; }
+    }
+
+    public class ConstructorInstance : Instance
+    {
+        public ConstructorInstance(Type serviceType, Type implementationType, ServiceLifetime lifetime) : base(serviceType, implementationType, lifetime)
+        {
+        }
+    }
+
+    public class LambdaInstance : Instance
+    {
+        public LambdaInstance(Type serviceType, Func<IServiceProvider, object> factory, ServiceLifetime lifetime) : base(serviceType, factory, lifetime)
+        {
+        }
+    }
+
+    public class ObjectInstance : Instance
+    {
+        public static ObjectInstance For<T>(T @object)
+        {
+            return new ObjectInstance(typeof(T), @object);
+        }
+        
+        public ObjectInstance(Type serviceType, object instance) : base(serviceType, instance)
+        {
+        }
+        
+        
+    }
+}
