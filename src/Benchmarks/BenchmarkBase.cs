f2b8bcf7b05d62d8d1e861dc47dfc799c7c55608
Jeremy D. Miller <jeremydmiller@yahoo.com>
2018-02-20 23:44:36 -0600

    wip: getting more work set up for the benchmarks

--- src/Benchmarks/BenchmarkBase.cs
+++ src/Benchmarks/BenchmarkBase.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using Baseline;
 using BlueMilk;
 using BlueMilk.Codegen;
+using BlueMilk.IoC.Instances;
 using BlueMilk.Microsoft.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,71 +13,79 @@ using StructureMap.AspNetCore;
 
 public class BenchmarkBase : IDisposable
 {
-    private IWebHost _blueMilkHost;
-    private IWebHost _smHost;
-    private IWebHost _aspnetHost;
+    protected IWebHost _blueMilkHost;
+
 
     public BenchmarkBase()
     {
         var builder = new WebHostBuilder();
         builder
             .UseBlueMilk()
+            .ConfigureServices(services =>
+            {
+                var registry = new ServiceRegistry();
+                configure(registry);
+
+                services.AddRange(registry);
+            })
             .UseUrls("http://localhost:5002")
             .UseServer(new NulloServer())
             .UseStartup<Startup>();
 
         _blueMilkHost = builder.Start();
 
-
-        var builder2 = new WebHostBuilder();
-        builder2
-            .UseUrls("http://localhost:5002")
-            .ConfigureServices(services =>
-            {
-                services.AddMvc();
-                services.AddLogging();
-            })
-            .UseServer(new NulloServer())
-            .UseStartup<Startup2>()
-            .UseStructureMap();
-
-        _smHost = builder2.Start();
-
-
-        var builder3 = new WebHostBuilder();
-        builder3
-            .UseUrls("http://localhost:5002")
-            .ConfigureServices(services =>
-            {
-                services.AddMvc();
-                services.AddLogging();
-            })
-            .UseServer(new NulloServer())
-            .UseStartup<Startup3>();
-
-        _aspnetHost = builder3.Start();
-
-        Types = _blueMilkHost.Services.As<Container>().Model.AllInstances.Select(x => x.ServiceType)
-            .Where(x => x.Assembly != typeof(Container).Assembly && !x.IsOpenGeneric())
-            .Where(x => x != typeof(IServiceProviderFactory<ServiceRegistry>))
-            .Where(x => !x.NameInCode().StartsWith("Func<"))
+        Instances = _blueMilkHost.Services.As<Container>().Model.AllInstances
+            .Where(x => x.ServiceType.Assembly != typeof(Container).Assembly && !x.ServiceType.IsOpenGeneric())
+            .Where(x => x.ServiceType != typeof(IServiceProviderFactory<ServiceRegistry>))
             .ToArray();
 
-        
 
-        Providers = new[] {_blueMilkHost.Services, _smHost.Services, _aspnetHost.Services};
+        Singletons = Instances.Where(x => x.Lifetime == ServiceLifetime.Singleton).Select(x => x.ServiceType)
+            .Distinct().ToArray();
+
+        Scoped = Instances.Where(x => x.Lifetime == ServiceLifetime.Scoped).Select(x => x.ServiceType)
+            .Distinct().ToArray();
+
+        Transients = Instances.Where(x => x.Lifetime == ServiceLifetime.Transient).Select(x => x.ServiceType)
+            .Distinct().ToArray();
+
+        Objects = Instances.OfType<ObjectInstance>().Select(x => x.ServiceType).Distinct().ToArray();
+
+        Lambdas = Instances.OfType<LambdaInstance>().Select(x => x.ServiceType).Distinct().ToArray();
+
+        Internals = Instances.Where(x => x.ImplementationType.IsNotPublic).Select(x => x.ServiceType).Distinct();
     }
 
-    public IServiceProvider BlueMilk => Providers[0];
-    public IServiceProvider StructureMap => Providers[1];
-    public IServiceProvider AspNetCore => Providers[2];
-    public IServiceProvider[] Providers { get; set; }
+    public IEnumerable<Type> Internals { get; set; }
+
+
+    public Type[] Lambdas { get; set; }
+
+    public Type[] Objects { get; set; }
+
+    public Type[] Transients { get; set; }
+
+    public Type[] Scoped { get; set; }
+
+    public Type[] Singletons { get; set; }
+
+    public Instance[] Instances { get; }
+
+    protected virtual void configure(ServiceRegistry services)
+    {
+        
+    }
+
+    public IServiceProvider BlueMilk => _blueMilkHost.Services;
+
     public Type[] Types { get; }
 
-    public void Dispose()
+    public virtual void Dispose()
     {
         _blueMilkHost?.Dispose();
-        _smHost?.Dispose();
-        _aspnetHost?.Dispose();
+
     }
+
+
 }
+
