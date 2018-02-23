86bb574e9c332ad64ef8e9590fb6b5a684cb415f
Randy Ridge <randy.ridge@hsn.net>
2018-02-23 16:01:19 -0500

    added test app with debugger.break

---
+++ WebApplication1/Program.cs
@@ -0,0 +1,17 @@
+﻿using BlueMilk.Microsoft.DependencyInjection;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebApplication1 {
+    public class Program {
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseBlueMilk()
+                .UseStartup<Startup>()
+                .Build();
+
+        public static void Main(string[] args) {
+            BuildWebHost(args).Run();
+        }
+    }
+}
