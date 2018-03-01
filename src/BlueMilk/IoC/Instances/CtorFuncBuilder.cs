787511c9517e8d5cfd76814e70b6b23ca4b7006b
Jeremy D. Miller <jeremydmiller@yahoo.com>
2018-03-01 09:07:54 -0600

    internalized FastExpressionCompiler

--- src/BlueMilk/IoC/Instances/CtorFuncBuilder.cs
+++ src/BlueMilk/IoC/Instances/CtorFuncBuilder.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
@@ -9,30 +7,13 @@ using Baseline;
 using BlueMilk.Codegen;
 using BlueMilk.IoC.Enumerables;
 using BlueMilk.Scanning.Conventions;
-using FastExpressionCompiler;
-using Microsoft.CodeAnalysis.VisualBasic.Syntax;
+using BlueMilk.Util;
 
 namespace BlueMilk.IoC.Instances
 {
     public static class CtorFuncBuilder
     {
-        static CtorFuncBuilder()
-        {
-            _coerceToList = typeof(CtorFuncBuilder).GetMethod(nameof(CoerceToList));
-            _coerceToArray = typeof(CtorFuncBuilder).GetMethod(nameof(CoerceArray));
-        }
-        
-        public static IList<T> CoerceToList<T>(object elements)
-        {
-            return elements.As<object[]>().OfType<T>().ToList();
-        }
-
-        public static T[] CoerceArray<T>(object elements)
-        {
-            return elements.As<object[]>().OfType<T>().ToArray();
-        }
-        
-        private static readonly Type[] _openTypes = new Type[]
+        private static readonly Type[] _openTypes =
         {
             typeof(Func<>),
             typeof(Func<,>),
@@ -51,22 +32,37 @@ namespace BlueMilk.IoC.Instances
             typeof(Func<,,,,,,,,,,,,,,>),
             typeof(Func<,,,,,,,,,,,,,,,>),
             typeof(Func<,,,,,,,,,,,,,,,,>)
-
         };
 
-        private static MethodInfo _coerceToList;
-        private static MethodInfo _coerceToArray;
+        private static readonly MethodInfo _coerceToList;
+        private static readonly MethodInfo _coerceToArray;
+
+        static CtorFuncBuilder()
+        {
+            _coerceToList = typeof(CtorFuncBuilder).GetMethod(nameof(CoerceToList));
+            _coerceToArray = typeof(CtorFuncBuilder).GetMethod(nameof(CoerceArray));
+        }
+
+        public static IList<T> CoerceToList<T>(object elements)
+        {
+            return elements.As<object[]>().OfType<T>().ToList();
+        }
+
+        public static T[] CoerceArray<T>(object elements)
+        {
+            return elements.As<object[]>().OfType<T>().ToArray();
+        }
 
         public static (Delegate func, Type funcType) LambdaTypeFor(Type concreteType, ConstructorInfo ctor)
         {
             return LambdaTypeFor(concreteType, concreteType, ctor);
         }
 
-        public static (Delegate func, Type funcType) LambdaTypeFor(Type serviceType, Type concreteType, ConstructorInfo ctor)
+        public static (Delegate func, Type funcType) LambdaTypeFor(Type serviceType, Type concreteType,
+            ConstructorInfo ctor)
         {
             var length = ctor.GetParameters().Length;
             var openType = _openTypes[length];
-
 
 
             var parameters = ctor.GetParameters();
@@ -74,8 +70,8 @@ namespace BlueMilk.IoC.Instances
             var ctorParams = new Expression[parameters.Length];
 
             var parameterTypes = new List<Type>();
-            
-            for (int i = 0; i < parameters.Length; i++)
+
+            for (var i = 0; i < parameters.Length; i++)
             {
                 var parameter = parameters[i];
 
@@ -88,7 +84,7 @@ namespace BlueMilk.IoC.Instances
                         var elementType = EnumerablePolicy.DetermineElementType(parameter.ParameterType);
                         var coerceMethod = (parameter.ParameterType.IsArray ? _coerceToArray : _coerceToList)
                             .MakeGenericMethod(elementType);
-                        
+
                         ctorParams[i] = Expression.Call(coerceMethod, arguments[i]);
                     }
                     else
@@ -109,16 +105,19 @@ namespace BlueMilk.IoC.Instances
             parameterTypes.Add(parameterType);
 
             var funcType = openType.MakeGenericType(parameterTypes.ToArray());
-            
-            if (funcType.MustBeBuiltWithFunc()) throw new InvalidOperationException($"The Func of signature {funcType.NameInCode()} is not publicly accessible for type {serviceType.FullNameInCode()}");
 
-            
+            if (funcType.MustBeBuiltWithFunc())
+                throw new InvalidOperationException(
+                    $"The Func of signature {funcType.NameInCode()} is not publicly accessible for type {serviceType.FullNameInCode()}");
+
+
             var callCtor = Expression.New(ctor, ctorParams);
 
             var lambda = Expression.Lambda(funcType, callCtor, arguments);
- 
+
             const int FastExpressionCompilerParameterLimit = 8;
-            if(parameterTypes.Count > FastExpressionCompilerParameterLimit) {
+            if (parameterTypes.Count > FastExpressionCompilerParameterLimit)
+            {
                 var bigAssFunc = lambda.Compile();
                 return (bigAssFunc, funcType);
             }
